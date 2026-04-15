2a1c7e7bcc75a3bacdd50a88fe83c474014378e9
Caique <52918563862@info.local>
2026-04-15 10:58:49 -0300

    Login feito

---
+++ vh-burguer/src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+return(
+    <>
+        <p>dshsuidsafdgg</p>
+    </>
+);
+}
+
+export default Home;
--- vh-burguer/src/pages/login/index.tsx
+++ vh-burguer/src/pages/login/index.tsx
@@ -1,29 +1,33 @@
 import { Fragment } from "react/jsx-runtime";
+import styles from './login.module.css'
+
 
 //Estrutura padrao
 const Login = () => {
     return (
         <>
-            <main>
-                <img src="" alt="" />
-                <aside>
-                    <h1>Login</h1>
-                    <form action="">
+            <main id={styles.main}>
+                <img src="../imgs/hamburguer-login.png" alt="Hámburguer flutuando com os ingredientes" />
 
-                        <div className="camp_form">
+                <div id={styles.campo_login}>
+                    <h1>Login</h1>
+                    <form id={styles.formulario}>
+
+                        <div className={styles.campo_form}>
                             <label htmlFor="email">Email</label>
                             <input type="email" name="email" placeholder="email@example.com" required />
                         </div>
 
-                        <div className="camp_form">
+                        <div className={styles.campo_form}>
                             <label htmlFor="senha">Senha</label>
                             <input type="password" name="senha" placeholder="********" required />
                         </div>
 
-                        <a href="">Esqueceu sua senha?</a>
+                        <a id={styles.esq_senha} href="">Esqueceu sua senha?</a>
                         <button type="submit">Entrar</button>
                     </form>
-                </aside>
+                </div>
+                
             </main>
         </>
     )
